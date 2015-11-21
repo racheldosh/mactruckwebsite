379046a02e5959449783cc54c095d1e2dc20a2f8
racheldoshcollins <rcollins@middlebury.edu>
2015-11-21 17:25:24 -0500

    update orderform.js

--- orderform.js
+++ orderform.js
@@ -1,4 +1,24 @@
-function calculatePrice(myform)
-{
-  var memPrice=myform.memoryItem.selectedIndex;
-}
+ function calculatePrice(){
+
+      //Get selected data  
+      var elt = document.getElementById("memoryItem");
+      var memory = elt.options[elt.selectedIndex].value;
+
+      elt = document.getElementById("hddItem");
+      var hdd = elt.options[elt.selectedIndex].value;
+
+      elt = document.getElementById("networkItem");
+      var network = elt.options[elt.selectedIndex].value;
+
+      //convert data to integers
+      memory = parseInt(memory);
+      hdd = parseInt(hdd);
+      network = parseInt(network);
+
+      //calculate total value  
+      var total = memory+hdd+network; 
+
+      //print value to  PicExtPrice 
+      document.getElementById("PicExtPrice").value=total;
+
+ }
