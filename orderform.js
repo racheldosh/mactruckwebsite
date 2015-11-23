e560def46ef41025444cba7eb1fd8369f3ea0fbe
racheldoshcollins <rcollins@middlebury.edu>
2015-11-23 17:16:49 -0500

    Update orderform.js

--- orderform.js
+++ orderform.js
@@ -1,7 +1,7 @@
  function calculatePrice(){
 
       //Get selected data  
-	  "use strict";
+ "use strict";
       var elt = document.getElementById("macChoice");
       var mac = elt.options[elt.selectedIndex].value;
 
@@ -10,14 +10,23 @@
 
       elt = document.getElementById("secondCond");
       var cond2 = elt.options[elt.selectedIndex].value;
+      
+      elt = document.getElementById("thirdCond");
+      var cond3 = elt.options[elt.selectedIndex].value;
+      
+      elt = document.getElementById("drink");
+      var drinkchoice = elt.options[elt.selectedIndex].value;
 
       //convert data to integers
       mac = parseInt(mac);
       cond1 = parseInt(cond1);
       cond2 = parseInt(cond2);
+      cond3 = parseInt(cond3);
+      drinkchoice = parseInt(drinkchoice);
+      
 
       //calculate total value  
-      var total = mac+cond1+cond2+.80; 
+      var total = mac+0.80+cond1+cond2+cond3+drinkchoice; 
 
       //print value to  PicExtPrice 
       document.getElementById("PicExtPrice").value = total.toFixed(2);
